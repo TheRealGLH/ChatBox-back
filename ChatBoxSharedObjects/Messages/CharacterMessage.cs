7cf0aec6a0cb0d5a3dd6b67d63d6230affabd927
TheRealGLH <10632069+TheRealGLH@users.noreply.github.com>
2023-12-11 14:49:09 +0100

    Serialises CharMessageType verbose

--- ChatBoxSharedObjects/Messages/CharacterMessage.cs
+++ ChatBoxSharedObjects/Messages/CharacterMessage.cs
@@ -1,9 +1,12 @@
-﻿namespace ChatBoxSharedObjects.Messages;
+﻿using System.Text.Json.Serialization;
+
+namespace ChatBoxSharedObjects.Messages;
 
 public class CharacterMessage
 {
     public string CharId { get; set; }
     public string OwnerId { get; set; }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public CharacterMessageType MessageType { get; set; }
 
     public CharacterMessage(String CharId, String OwnerId, CharacterMessageType messageType){

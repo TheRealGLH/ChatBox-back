81d015306801a4fddba598a7780471f294a2f8ed
TheRealGLH <10632069+TheRealGLH@users.noreply.github.com>
2023-12-14 11:43:46 +0100

    RabbitMQ and ProfileDB in docker + configs

--- ChatBoxSharedObjects/Settings/RabbitMqSettings.cs
+++ ChatBoxSharedObjects/Settings/RabbitMqSettings.cs
@@ -3,6 +3,7 @@ namespace ChatBoxSharedObjects.Settings;
 public class RabbitMqSettings
 {
     public string HostName { get; set; }
+    public string Port { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
     public string ExchangeName { get; set; }
